9e833c210243e6b233b66093ba22cd0d9026ca87
Kon <yem_salat@hotmail.com>
2015-09-15 00:19:15 +1000

    Added special logic

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -13,6 +13,14 @@
     $('#update fieldset.tabular legend').click(function() {
       $(this).closest('fieldset').toggleClass('show-children');
     });
+
+    // redirect
+    // CHECK ADMIN
+    if ( $('#top-menu a.administration').length ) {
+      $('$top-menu>ul>li').eq(0).hide();
+      $('$top-menu>ul>li').eq(3).hide();
+      $('$top-menu>ul>li').eq(4).hide();
+    }
   });
 
 })(jQuery);
